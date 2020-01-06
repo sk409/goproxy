37ecc07eb3bd18f5cb51aa36f0695fe6f1e75668
Shoutokobayashi <deepstrap353@gmail.com>
2020-01-06 11:15:48 +0900

    dev

--- http_proxy.go
+++ http_proxy.go
@@ -10,6 +10,7 @@ import (
 	"crypto/x509/pkix"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
 	"math/big"
 	mrand "math/rand"
@@ -58,6 +59,7 @@ func NewHTTPProxy(certfile, keyfile string) (*HTTPProxy, error) {
 }
 
 func (p *HTTPProxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(p.Hooks.Request)
 	if p.Hooks.Request != nil {
 		p.Hooks.Request(r)
 	}
